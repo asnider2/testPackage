91a8a5fbfa548731c00e845df8f16547e000d00c
asnider2 <123927336+asnider2@users.noreply.github.com>
2024-04-03 00:58:07 -0400

    add all functs to myfun

--- R/myfunc.r
+++ R/myfunc.r
@@ -12,3 +12,20 @@ myfunc <- function(a, b) {
   result <- a * b + a
   return(result)
 }
+estimate_beta <- function(y, X) {
+  n <- length(y)
+  X <- cbind(1, X)
+  beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
+  return(beta_hat)
+}
+my_theme <- function() {
+  theme(
+    # Adjust axis line color
+    axis.line = element_line(color = "white"),
+    # Adjust major and minor grid line color
+    panel.grid.major = element_line(color = "pink"),
+    panel.grid.minor = element_line(color = "purple"),
+    # Adjust panel background color
+    panel.background = element_rect(fill = "black")
+  )
+}
